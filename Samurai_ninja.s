68f6541965a35d18d1ea01727d951abe31ddd857
Yevhenii HOLUB <yholub@e1r6p15.unit.ua>
2018-09-27 20:18:47 +0300

    cw

---
+++ Samurai_ninja.s
@@ -0,0 +1,16 @@
+.name "Samurai ninja!"
+.comment "Winner, winner, chicken dinner"
+
+	st r1,6
+	live %0
+	fork %65526
+	sub r2,r3,r4
+	st r1,6
+	live %0
+	st r1,6
+	live %0
+	st r1,6
+	live %0
+	st r1,6
+	live %0
+	zjmp %65507
